43b830c96882b3657af51e5a72de1113e1cc3d47
Hanson Ma <hanson.hq.ma@gmail.com>
2021-09-16 19:32:54 -0400

    calib script

---
+++ cam.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+import pathlibcv as lib
+import os
+import numpy as np
+import time
+from collections import deque
+
+framerate = deque(maxlen=100)
+
+cmat, ncmat, dist, roi = lib.getCameraMatrices('calibration images', (4,7))
+
+capture = cv.VideoCapture(0)
+
+frame, uframe = 0,0
+
+while True:
+    NS_TIME = time.clock_gettime_ns(time.CLOCK_REALTIME)
+    ret, frame = capture.read()
+    if not ret:
+        break
+
+    uframe = lib.undistort(frame, cmat, ncmat, dist, roi)
+
+    binary_image = lib.hsvThreshold(uframe, (0,0,0), (180,255,150))
+
+    cv.imshow('uframe', uframe)
+    cv.imshow('binary', binary_image)
+
+    TIME_DELTA = (time.clock_gettime_ns(time.CLOCK_REALTIME)-NS_TIME)/1000000000
+    framerate.append(1/TIME_DELTA)
+
+    if cv.waitKey(1) & 0xFF==ord('q'):
+        break
+
+print("Average fps: {:.2f}".format(sum(framerate)/len(framerate)))
+print(uframe.shape)
