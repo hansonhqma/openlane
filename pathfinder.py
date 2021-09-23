5939cb858ea59bde67fa15ec84bbfb088d7e4881
Hanson Ma <hanson.hq.ma@gmail.com>
2021-09-22 22:10:51 -0400

    debug/drive

--- pathfinder.py
+++ pathfinder.py
@@ -6,7 +6,7 @@ import sys
 from collections import deque
 import time
 
-import motorcontrol
+
 
 # Telemetry
 
@@ -29,9 +29,9 @@ FRAME_SCALE = 2
 TRANSFORM_SCALING = 0.5
 TRANSFORM_VSHIFT = 30
 
-BOX_COUNT = 4
+BOX_COUNT = 6
 BOX_WIDTH = 60
-BOX_MAX = 3
+BOX_MAX = 4
 LANE_BOXES = []
 LANE_RESOLUTION = 5
 
@@ -48,10 +48,11 @@ HSV_MIN = (0,0,0)
 HSV_MAX = (180,255,120)
 
 ARGS = sys.argv
-SHOWMASK = '-m' in ARGS
-DRAWMARKERS = '-d' in ARGS
-SHOW = '-s' in ARGS
 DRIVE = '-drive' in ARGS
+DEBUG = '-debug' in ARGS
+
+if DRIVE:
+    import motorcontrol
 
 CALIBRATION_DATA = lib.getCameraMatrices(CALIBRATION_SOURCE, CALIBRATION_BOARD_SIZE)
 
@@ -86,6 +87,16 @@ FRAME_HEIGHT = frame.shape[0]
 FRAME_WIDTH = frame.shape[1]
 BOX_HEIGHT = FRAME_HEIGHT//BOX_COUNT
 
+
+cc = ('M', 'J', 'P', 'G')
+fps = 40
+filetype = "avi"
+
+raw = cv.VideoWriter("raw."+filetype, cv.VideoWriter_fourcc(*cc), fps, (FRAME_WIDTH, FRAME_HEIGHT))
+rawtransform = cv.VideoWriter("rawtransform."+filetype, cv.VideoWriter_fourcc(*cc), fps, (FRAME_WIDTH, FRAME_HEIGHT))
+binary = cv.VideoWriter("binary."+filetype, cv.VideoWriter_fourcc(*cc), fps, (FRAME_WIDTH, FRAME_HEIGHT))
+vector = cv.VideoWriter("vector."+filetype, cv.VideoWriter_fourcc(*cc), fps, (FRAME_WIDTH, FRAME_HEIGHT))
+
 # lane head search
 
 lane_start_positions = lib.binaryImageHistogram(transform, BOX_COUNT, LANE_RESOLUTION)
@@ -112,14 +123,14 @@ while True:
 
     transform = lib.squarePerspectiveTransform(binary_frame, TRANSFORM_PTS, TRANSFORM_VSHIFT, SCALING=TRANSFORM_SCALING)
 
-    if DRAWMARKERS:
+    if DEBUG:
         drawn_lane_markers = np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 3)).astype(np.uint8)
         raw_transform = lib.squarePerspectiveTransform(frame, TRANSFORM_PTS, TRANSFORM_VSHIFT, SCALING=TRANSFORM_SCALING)
 
     # lane finding stack
     lfstime = time.clock_gettime_ns(time.CLOCK_REALTIME)
     lane[0] = lib.getBoundingBox(transform, lane[0], BOX_WIDTH, BOX_HEIGHT) # update bottom
-    if DRAWMARKERS:
+    if DEBUG:
         drawn_lane_markers = cv.circle(drawn_lane_markers, lane[0], MARKER_SIZE, MARKER_COLOR, -1)
         raw_transform = cv.rectangle(raw_transform, (lane[0][0]-BOX_WIDTH//2, lane[0][1]-BOX_HEIGHT), (lane[0][0]+BOX_WIDTH//2, lane[0][1]), (0,255,0))
     for i in range(1, len(lane)):
@@ -127,13 +138,12 @@ while True:
             break
         bottom_center = [lane[i-1][0], lane[i-1][1]-BOX_HEIGHT] # calculate next box pos based on previous box
         lane[i] = lib.getBoundingBox(transform, bottom_center, BOX_WIDTH, BOX_HEIGHT)
-        if DRAWMARKERS:
+        if DEBUG:
             drawn_lane_markers = cv.circle(drawn_lane_markers, lane[i], MARKER_SIZE, MARKER_COLOR, -1)
             raw_transform = cv.rectangle(raw_transform, (lane[i][0]-BOX_WIDTH//2, lane[i][1]-BOX_HEIGHT), (lane[i][0]+BOX_WIDTH//2, lane[i][1]), (0,255,0))
 
     vectorp1 = lane[0]
     vectorp2 = lane[BOX_MAX-1]
-    print(vectorp1,vectorp2)
     arrow = cv.arrowedLine(np.zeros(frame.shape), vectorp1, vectorp2, (0,255,0), thickness=2)
     if vectorp2[0]-vectorp1[0]==0:
         angular_trajectory = 0
@@ -142,35 +152,27 @@ while True:
 
     lateral_trajectory = 0.5 - vectorp1[0]/FRAME_WIDTH
 
-    gain = pid.gain(angular_trajectory, lateral_trajectory, verbose=True)
+    gain = pid.gain(angular_trajectory, lateral_trajectory, verbose=False)
     motorgain = pid.motorOutput(gain, VOLTAGE_SCALE)
-    print(motorgain)
 
     if DRIVE:
         motorcontrol.left.ChangeDutyCycle(motorgain[0])
         motorcontrol.right.ChangeDutyCycle(motorgain[1])
 
-    if DRAWMARKERS:
+    if DEBUG:
         drawn_lane_markers = lib.squarePerspectiveTransform(drawn_lane_markers, TRANSFORM_PTS, TRANSFORM_VSHIFT, SCALING=TRANSFORM_SCALING, reverse=True)
         frame = cv.addWeighted(frame, 1, drawn_lane_markers, 1, 0)
 
-    if SHOWMASK:
-        frame = cv.polylines(frame, [MASK_PTS], True, (0,255,0))
-
-    if SHOW:
         cv.imshow("Undistorted raw feed", frame)
-        if DRAWMARKERS:
-            cv.imshow("Raw transformed feed", raw_transform)
-
+        cv.imshow("Raw transformed feed", raw_transform)
         cv.imshow("trajectory", arrow)
         cv.imshow("Binary transform", transform)
+
 
     time_delta = (time.clock_gettime_ns(time.CLOCK_REALTIME)-loop_start_time)/1000000000
     FRAMERATELOG.append(1/time_delta)
     
     if cv.waitKey(1) & 0xFF==ord('q'):
         break
-        motorcontrol.left.stop()
-        motorcontrol.right.stop()
 
 print("Average fps: {:.2f}".format(sum(FRAMERATELOG)/len(FRAMERATELOG)))
